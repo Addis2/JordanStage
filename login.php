6de934d72cf17bc31835fd4d84fc57258e373ccd
Addis2 <83008579+Addis2@users.noreply.github.com>
2022-09-19 10:57:50 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -3,12 +3,12 @@
     session_start();
     $message="";
     if(count($_POST)>0) {
-        $query = "SELECT * FROM `gebruikers` WHERE `Gebruiks_naam` = '".$_POST["username"]."' AND `Wachtwoord` = '".$_POST["password"]."'";
+        $query = "SELECT * FROM `Jordan` WHERE `gebruiksnaam` = '".$_POST["gebruikersnaam"]."' AND `wachtwoord` = '".$_POST["wachtwoord"]."'";
         $result = mysqli_query($db_connect, $query);
         if(mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_array($result);
             $_SESSION["ID"] = $row['ID'];
-            $_SESSION["name"] = $row['Gebruiks_naam'];
+            $_SESSION["name"] = $row['gebruikersnaam'];
         } else {
             $message = "Invalid Username or Password!";
         }
