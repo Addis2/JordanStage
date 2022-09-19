68630f03f848255e22822f646da88e8a4660753f
Addis2 <83008579+Addis2@users.noreply.github.com>
2022-09-19 11:55:54 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -1,19 +1,21 @@
 <?php
-    require 'dbconnect.php';
-    session_start();
-    $message="";
-    if(count($_POST)>0) {
-        $query = "SELECT * FROM `Jordan` WHERE `1` = '".$_POST["gebruikersnaam"]."' AND `wachtwoord` = '".$_POST["password"]."'";
-        $result = mysqli_query($db_connect, $query);
-        if(mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_array($result);
-            $_SESSION["ID"] = $row['ID'];
-            $_SESSION["name"] = $row['gebruiksnaam'];
-        } else {
-            $message = "Invalid Username or Password!";
-        }
-    }
-    if(isset($_SESSION["ID"])) {
-        return header("Location:index.html");
-    }
+
+echo " This is test";
+    // require 'dbconnect.php';
+    // session_start();
+    // $message="";
+    // if(count($_POST)>0) {
+    //     $query = "SELECT * FROM `Jordan` WHERE `gebruikersnaam` = '".$_POST["gebruikersnaam"]."' AND `wachtwoord` = '".$_POST["password"]."'";
+    //     $result = mysqli_query($db_connect, $query);
+    //     if(mysqli_num_rows($result) > 0) {
+    //         $row = mysqli_fetch_array($result);
+    //         $_SESSION["ID"] = $row['ID'];
+    //         $_SESSION["name"] = $row['gebruiksnaam'];
+    //     } else {
+    //         $message = "Invalid Username or Password!";
+    //     }
+    // }
+    // if(isset($_SESSION["ID"])) {
+    //     return header("Location:index.html");
+    // }
 ?>
