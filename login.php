01ab9d7d9a2ac47be0000af028cedb53ee3a06d0
Addis2 <83008579+Addis2@users.noreply.github.com>
2022-09-19 11:19:12 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -1,19 +1,82 @@
 <?php
-    require 'dbconnect.php';
-    session_start();
-    $message="";
-    if(count($_POST)>0) {
-        $query = "SELECT * FROM `Jordan` WHERE `gebruiksnaam` = '".$_POST["gebruikersnaam"]."' AND `wachtwoord` = '".$_POST["password"]."'";
-        $result = mysqli_query($db_connect, $query);
-        if(mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_array($result);
-            $_SESSION["ID"] = $row['ID'];
-            $_SESSION["name"] = $row['gebruikersnaam'];
-        } else {
-            $message = "Invalid Username or Password!";
+
+require 'config.php';
+session_start();
+$message="";
+
+if(count($_POST)>0) {
+
+    $db_host = 'localhost';
+    $db_user = 'Jordan';
+    $db_pass = 'YaredDaniel1126';
+    $db_data = 'Datas';
+
+    $con = mysqli_connect($db_host, $db_user, $db_pass, $db_data);
+    $username = $_POST["gebruikersnaam"];
+    $password = $_POST["wachtwoord"];
+
+    $stmt = $con->prepare("SELECT * FROM Jordan WHERE gebruikersnaam=? AND password=md5(?);");
+    $stmt->bind_param("ss", $gebruikernaam, $wachtwoord);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+
+    $result = mysqli_query($mysqli,"SELECT * FROM login_user WHERE gebruikersnaam='" . $_POST["gebruikersnaam"] . "' and wachtwoord = '". $_POST["wachtwoord"]."'");
+    $row  = mysqli_fetch_array($result);
+    if(is_array($row)) {
+        $_SESSION["id"] = $row['id'];
+        $_SESSION["name"] = $row['name'];
+        $_SESSION["role"] = $row['role'];
+        if ($_SESSION["role"] == "admin") {
+            header("Location:index.html");
+
         }
+    } else {
+        $message = "De gegevens die je hebt ingevuld zijn fout";
     }
-    if(isset($_SESSION["ID"])) {
-        return header("Location:index.html");
-    }
+}
+
+
 ?>
+
+<html>
+<head>
+    <title>Usesdasdr Login</title>
+</head>
+<body>
+<form name="frmUser" method="post" action="" align="center">
+    <div class="message"><?php if($message!="") { echo $message; } ?></div>
+    <div class="div1">
+
+        <h3 align= "center">Vul logingegevens in</h3>
+        Gebruikersnaam:<br>
+        <input type="text" name="user_name">
+        <br>
+        Wachtwoord:<br>
+        <input type="password" name="password">
+        <br><br>
+        <input class="button" type="submit" name="submit" value="Submit" class="button">
+    </div>
+</form>
+<style>
+    body{
+        background-color: antiquewhite;
+    }
+    .div1{
+        background-color: white;
+        text-align:center;
+        font-size: 30px;
+        max-width: 50%;
+        box-shadow: 5px 5px black;
+        background-color:  #C8BCAC;
+    }
+    .button{
+        background-color: grey;
+        height: 40px;
+        width: 100px;
+        border: 2px solid black;
+    }
+
+</style>
+</body>
+</html>
